670898161b86aff04dd1dbded8dc7b97aa22fd88
yurkovskiy <yurkovskiy@gmail.com>
2015-11-01 19:38:30 +0200

    Added block definition with params

--- ex_006_blocks.rb
+++ ex_006_blocks.rb
@@ -7,3 +7,18 @@ end
 doSomething {
 	puts "Inside the doSomething BLOCK"
 }
+
+def doSome
+	puts "Inside the doSome METHOD"
+	yield 200, 300
+end
+
+doSome {
+	|x, y|
+	puts "You are in the BLOCK doSome with params #{x} and #{y}"
+}
+
+def main
+	doSomething
+	doSome	
+end
