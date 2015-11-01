642d3b3b1329e71d7a17d2809d16e04e204d7091
yurkovskiy <yurkovskiy@gmail.com>
2015-11-01 17:06:29 +0200

    Added example of control flow operators: ex_003

---
+++ ex_003_control.rb
@@ -0,0 +1,16 @@
+# Control flow operators example
+=begin
+if...else statement, unless, case
+=end
+# Here an example
+print "Please enter number "
+num = gets
+num = num.to_i
+if num < 0
+	puts "You entered number that less than zero"
+elsif num >= 0 and num <= 5
+	puts "You entered number between 0 and 5"
+else 
+	puts "You entered number that more bigger than 5"
+end
+		
