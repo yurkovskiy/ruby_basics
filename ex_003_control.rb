05f91c81bc6b8729032eeaafa6af176170869e9e
yurkovskiy <yurkovskiy@gmail.com>
2015-11-01 17:11:37 +0200

    Added unless example into ex_003

--- ex_003_control.rb
+++ ex_003_control.rb
@@ -3,6 +3,10 @@
 if...else statement, unless, case
 =end
 # Here an example
+
+puts "if example"
+
+# if
 print "Please enter number "
 num = gets
 num = num.to_i
@@ -13,4 +17,14 @@ elsif num >= 0 and num <= 5
 else 
 	puts "You entered number that more bigger than 5"
 end
+# end of if
+
+puts "unless example"
+unless num < 0
+	puts "You entered number that bigger than 0"
+else
+	puts "You entered number that lower than 0"
+end
+
+# unless
 		
