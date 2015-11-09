4fd852eddd403662096a56c89cd2fba5aee61269
yurkovskiy <yurkovskiy@gmail.com>
2015-11-09 19:02:17 +0200

    Added Shape and Rectangle classes

---
+++ Shape.rb
@@ -0,0 +1,25 @@
+module Geometry
+	class Shape
+		def initialize(width, height)
+			@width = width
+			@height = height
+		end
+		def get_width
+			@width			
+		end
+		def get_height
+			@height			
+		end
+	end
+
+	class Rectangle < Shape
+		def length
+			2 * (@width + @height)
+		end
+		def area
+			@width * @height			
+		end
+
+	end
+end
+
---
+++ main.rb
@@ -0,0 +1,4 @@
+# main file
+require_relative "Shape"
+Rect = Geometry::Rectangle.new(20, 30)
+puts "Length of rectangle #{Rect.length}"
